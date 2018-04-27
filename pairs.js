5c6d4a2af209f2cab50c07c8257cb52f0c92ce2f
Ryan Ulibarri <38776589+rulibar@users.noreply.github.com>
2018-04-26 22:22:53 -0700

    Added notes to pairs.js
    Notes explain the structure and usage of pairs.js

--- pairs.js
+++ pairs.js
@@ -1,3 +1,32 @@
+/*
+
+stores the list of pairs we will be watching and storing data for in the
+following format
+
+list = {
+    "exchange1": {
+        "base1": [
+            "asset1",
+            "asset2"
+        ],
+        "base2": [
+            "asset1",
+            "asset2"
+        ]
+    },
+    "exchange2": {
+        ...
+    }
+}
+
+Notes:
+- We should be able to update this while scanner is running so that we can leave
+  scanner running 24/7
+- Keep this list simple for now. Let's just track one pair while we get the
+  core functionality added (Bitfinex BTCUSD)
+
+*/
+
 list = {
     "Bitfinex": {
         "USD": [
