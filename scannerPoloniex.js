035779b078b2f23c2b1df89a94792b9e79a37222
Ryan Ulibarri <38776589+rulibar@users.noreply.github.com>
2018-05-05 15:50:28 -0700

    Update scannerPoloniex.js
    Rename startDate to tsStart, todayDate to ts (timestamp), made tsStart a constant, and removed data var. Retrieve pairs and get ts after the ticker data comes in

--- scannerPoloniex.js
+++ scannerPoloniex.js
@@ -9,14 +9,13 @@ const Poloniex = require('poloniex-api-node')
 // vars
 const interval = 15 // seconds between checks
 const poloniex = new Poloniex()
-var startDate = (new Date()).getTime()
-var todayDate
-var data
+const tsStart = (new Date()).getTime()*1000
 
 // functions
 function _getTick () {
     poloniex.returnTicker((err, ticker) => {
         if (err) throw err
+        ts = (new Date()).getTime()*1000
         pairs = JSON.parse(fs.readFileSync('pairsPoloniex.json'))
         for (base in pairs) {
             for (i in pairs[base]) {
