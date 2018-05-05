b16fbf46e18cadbe13854ba6089f9f974b71c695
Ryan Ulibarri <38776589+rulibar@users.noreply.github.com>
2018-05-04 21:51:36 -0700

    Update scannerPoloniex.js
    Can now update pairsPoloniex.json while running scannerPoloniex.js

--- scannerPoloniex.js
+++ scannerPoloniex.js
@@ -1,3 +1,27 @@
 /*
 scannerPoloniex.js
 */
+
+// imports
+const fs = require('fs')
+
+// vars
+const interval = 15 // seconds between checks
+var startDate = (new Date()).getTime()
+var todayDate
+var data
+
+// functions
+function _getTick () {
+    todayDate = (new Date()).getTime()
+    data = JSON.parse(fs.readFileSync('pairsPoloniex.json'))
+    console.log("Pairs recieved")
+    secondsSinceStart = (todayDate - startDate)/1000
+    console.log(JSON.stringify(data))
+}
+
+// main
+_getTick()
+setInterval(function () {
+    _getTick()
+}, interval*1000)
