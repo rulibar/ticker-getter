534cf2ba52d2232ed7bf836b1c4609d889959995
Ryan Ulibarri <38776589+rulibar@users.noreply.github.com>
2018-06-13 09:33:35 -0700

    Create scannerBinance.js

---
+++ scannerBinance.js
@@ -0,0 +1,6 @@
+/*
+scannerBinance.js
+Description: A candle data compiler and saver for Binance exchange. Periodically
+cycles through the pairs in pairsBinance.json, compile candle data, and store to
+the Data file. See README.md
+*/
