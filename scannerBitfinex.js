3590960d0ecd867db387bdef341e6eaa8146d902
Ryan Ulibarri <38776589+rulibar@users.noreply.github.com>
2018-06-17 13:18:23 -0700

    Update scannerBitfinex.js to v1.0.9
        /- fix bug where it says Gdax WS closed
        /- import ./tgLib.js
        /- store PACKAGE, NAME, VERSION, TS_START in tgLib
        /- store _outLog in tgLib as outLog
        /- most var tsLastLog to tgLib
        /- move _startupMessage to tgLib as startupMessage
        /- move over LOG_INTERVAL
        /- move over SAVE_INTERVAL
        /- move over _getSubs and call from lib
        /- move over _saveCandles and call from lib
        /- move over subs and import with lib
        /- move over _onTrade function as onTrade
        /- rename _openWebSocket to openWS
        /- rename _setWSMethods to setWSMethods
        /- stop importing fs

--- scannerBitfinex.js
+++ scannerBitfinex.js
@@ -32,200 +32,64 @@ v1.0
     /- copy _onTrade from scannerGdax and call in onTradeEntry
     /- redo the WS programming to function like Binance and Gdax rather than
        Poloniex (see notesRyan)
+    v1.0.9
+    /- fix bug where it says Gdax WS closed
+    /- import ./tgLib.js
+    /- store PACKAGE, NAME, VERSION, TS_START in tgLib
+    /- store _outLog in tgLib as outLog
+    /- most var tsLastLog to tgLib
+    /- move _startupMessage to tgLib as startupMessage
+    /- move over LOG_INTERVAL
+    /- move over SAVE_INTERVAL
+    /- move over _getSubs and call from lib
+    /- move over _saveCandles and call from lib
+    /- move over subs and import with lib
+    /- move over _onTrade function as onTrade
+    /- rename _openWebSocket to openWS
+    /- rename _setWSMethods to setWSMethods
+    /- stop importing fs
 
 */
 
 // imports
-var fs = require('fs'),
+var lib = require('./tgLib'),
     Bitfinex = require('bitfinex-api-node')
 
-const PACKAGE = JSON.parse(fs.readFileSync('package.json')),
-    NAME = PACKAGE.name,
-    VERSION = PACKAGE.version,
-    SAVE_INTERVAL = 15, // candle size in seconds
-    LOG_INTERVAL = 60*60*24, // hr output period in seconds
-    TS_START = (new Date()).getTime(), // starting time in ms
-    PAIRSFILE = "pairsBitfinex.json", // name of file where pairs are stored
+const PAIRSFILE = "pairsBitfinex.json", // name of file where pairs are stored
     EXCHANGE = "Bitfinex" // exchange name
-var BITFINEX = new Bitfinex(),
-    tsLast = {}, // object storing last timestamp for each pair
-    tsLastLog = 0, // integer storing the timestamp of the last log update
-    priceLast = {}, // object storing last price for each pair
-    trades = {}, // object containing lists of trades used to compile the next candles
-    subs = [] // list of currency pairs that im currently subscribed too
-
-// functions
-function _outLog() {
-    let hr = "#############",
-        ts = (new Date()).getTime(),
-        timeRunning = ts - TS_START,
-        daysRunning = timeRunning / (1000 * 60 * 60 * 24),
-        timeSinceLast = ts - tsLastLog
-    // get hr
-    if (tsLastLog == 0) {
-        hr = `${hr} ${NAME} v${VERSION} started up ${hr}`
-    } else {
-        hr += "#####"
-        if (timeSinceLast/1000 > LOG_INTERVAL) {
-            hr = `${hr} days since start: ${daysRunning.toFixed(2)} ${hr}`
-        } else return
-    }
-    // output log and update tsLastLog
-    console.log(hr)
-    tsLastLog = ts
-}
-
-function _startupMessage(exchange) {
-    console.log(`${exchange} WebSocket open.`)
-    setTimeout(() => {
-        console.log("Now collecting and storing candle data.")
-    }, 3000)
-}
-
-function _getSubs(filename) {
-    // update subscriptions from json file
-    // - get pairs from json then newSubs from pairs
-    let pairs = JSON.parse(fs.readFileSync(filename)),
-        newSubs = [],
-        pairToCurrencyPair = {}
-    for (base in pairs) {
-        for (i in pairs[base]) {
-            let asset = pairs[base][i],
-                currencyPair = base+"_"+asset,
-                pair = asset+base
-            pairToCurrencyPair[pair] = currencyPair
-            if (newSubs.indexOf(pair) < 0) {
-                newSubs.push(pair)
-            }
-        }
-    }
-    // - compare newSubs to subs
-    // -- unsub outdated subs
-    for (i in subs) {
-        let pair = subs[i],
-            currencyPair = pairToCurrencyPair[pair]
-        if (newSubs.indexOf(pair) < 0) {
-            delete trades[pair]
-            delete tsLast[pair]
-            delete priceLast[pair]
-            subs.splice(i, 1)
-            console.log(`Removed subscription to ${pair}.`)
-        }
-    }
-    // -- add new subs
-    for (i in newSubs) {
-        let pair = newSubs[i],
-            currencyPair = pairToCurrencyPair[pair]
-        if (subs.indexOf(pair) < 0) {
-            trades[pair] = []
-            tsLast[pair] = (new Date()).getTime()
-            priceLast[pair] = 0
-            subs.push(pair)
-            console.log(`Subscribed to ${pair}.`)
-        }
-    }
-}
-
-function _saveCandles(exchange) {
-    // convert trade data to candles and save to storage
-    // - backup trades, reset trades
-    let tradeData = {}
-    for (pair in trades) {
-        tradeData[pair] = trades[pair]
-        trades[pair] = []
-    }
-    // - cycle through subs
-    for (i in subs) {
-        let pair = subs[i],
-            // get timestamps
-            ts1 = tsLast[pair],
-            ts2 = (new Date()).getTime(),
-            // get trades for this pair only
-            tradeList = tradeData[pair],
-            // initialize ohlc and volume
-            ohlc = [0, 0, 0, 0],
-            volume = 0
-        tsLast[pair] = ts2
-        // get ohlc and volume
-        if (tradeList.length == 0) {
-            if (priceLast[pair] == 0) continue
-            let price = priceLast[pair]
-            ohlc = [price, price, price, price]
-        } else {
-            ohlc = [
-                tradeList[0].price,
-                tradeList[0].price,
-                tradeList[0].price,
-                tradeList[tradeList.length - 1].price
-            ]
-            for (j in tradeList) {
-                if (tradeList[j].price > ohlc[1]) ohlc[1] = tradeList[j].price
-                if (tradeList[j].price < ohlc[2]) ohlc[2] = tradeList[j].price
-                volume += tradeList[j].amount
-            }
-        }
-        // get candle
-        let candle = `${ts1},${ts2},`
-        for (j in ohlc) candle += `${ohlc[j]},`
-        candle += volume
-        // get path
-        let date = new Date(ts2),
-            day = date.getDate(),
-            month = date.getMonth() + 1,
-            year = date.getFullYear(),
-            path = "",
-            items = [`Data/`, `${exchange}/`, `${pair}/`, `${year}/`],
-            filename = `${month}-${day}.csv`
-        // make sure path exists one level at a time
-        for (i in items) {
-            path += items[i]
-            if (!fs.existsSync(path)) fs.mkdirSync(path)
-        }
-        path += filename
-        // append candle to path
-        fs.appendFileSync(path, candle + "\n")
-    }
-}
+var BITFINEX = new Bitfinex()
 
 // initialize websocket
-function _onTrade(pair, amount, price) {
-    trades[pair].push({
-        amount: amount,
-        price: price
-    })
-    priceLast[pair] = price
-}
-
-function _setWSMethods(WS) {
+function setWSMethods(WS) {
     WS.on('error', (err) => {
         console.log("Error:")
         console.log(err)
     })
 
     WS.on('open', () => {
-        _startupMessage(EXCHANGE)
+        lib.startupMessage(EXCHANGE)
     })
 
     WS.on('close', (res) => {
-        console.log("Gdax WebSocket closed.")
+        console.log("Bitfinex WebSocket closed.")
         if (res == undefined) res = "No response from server."
         console.log(res)
         console.log("Trying to reopen the WebSocket...")
-        setTimeout(() => _openWebSocket(), 1000)
+        setTimeout(() => openWS(), 1000)
     })
 
     WS.onTradeEntry({}, (data, info) => {
         let pair = info.pair
-        if (subs.indexOf(pair) > -1) {
+        if (lib.subs.indexOf(pair) > -1) {
             let trade = data[0],
                 amount = Math.abs(trade[2]),
                 price = trade[3]
-            _onTrade(pair, amount, price)
+            lib.onTrade(pair, amount, price)
         }
     })
 }
 
-function _openWebSocket() {
+function openWS() {
     BITFINEX = (new Bitfinex()).rest()
     // get a list of pairs
     BITFINEX.symbols((err, symbols) => {
@@ -233,7 +97,7 @@ function _openWebSocket() {
         for (i in symbols) symbols[i] = symbols[i].toUpperCase()
         // set up WS
         BITFINEX = (new Bitfinex()).ws()
-        _setWSMethods(BITFINEX)
+        setWSMethods(BITFINEX)
         BITFINEX.on('open', () => {
             for (i in symbols) BITFINEX.subscribeTrades(symbols[i])
         })
@@ -242,15 +106,15 @@ function _openWebSocket() {
 }
 
 // script
-_outLog()
-_getSubs(PAIRSFILE)
-_openWebSocket()
+lib.outLog()
+lib.getSubs(PAIRSFILE)
+openWS()
 // - messages from initializing WS will appear here
 
 // - start collecting
 setInterval(() => {
     // save candles and refresh subscriptions every SAVE_INTERVAL
-    _outLog()
-    _saveCandles(EXCHANGE)
-    _getSubs(PAIRSFILE)
-}, SAVE_INTERVAL * 1000)
+    lib.outLog()
+    lib.saveCandles(EXCHANGE)
+    lib.getSubs(PAIRSFILE)
+}, lib.SAVE_INTERVAL * 1000)
