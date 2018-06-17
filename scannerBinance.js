1ae3cf67cc84163ce50301b6cc29142dc4084414
Ryan Ulibarri <38776589+rulibar@users.noreply.github.com>
2018-06-17 13:17:43 -0700

    Update scannerBinance.js to v1.0.9
        /- import tgLib as lib
        /- remove all constants except PAIRSFILE and EXCHANGE and call from lib
        /- remove tsLastLog
        /- remove _outLog and _startupMessage and call from lib
        /- remove tsLast, priceLast, trades, subs
        /- remove _getSubs and _saveCandles and call from lib
        /- remove _onTrade and call from lib
        /- rename _openWebSocket to openWS
        /- stop importing fs

--- scannerBinance.js
+++ scannerBinance.js
@@ -30,171 +30,29 @@ v1.0
     /- copy _onTrade from scannerGdax
     /- remove _onMessage and use _onTrade instead
     /- refer to BINANCE as a var instead of constant
+    v1.0.9
+    /- import tgLib as lib
+    /- remove all constants except PAIRSFILE and EXCHANGE and call from lib
+    /- remove tsLastLog
+    /- remove _outLog and _startupMessage and call from lib
+    /- remove tsLast, priceLast, trades, subs
+    /- remove _getSubs and _saveCandles and call from lib
+    /- remove _onTrade and call from lib
+    /- rename _openWebSocket to openWS
+    /- stop importing fs
 
 */
 
 // imports
-var fs = require('fs'),
+var lib = require('./tgLib'),
     Binance = require('node-binance-api')
 
-const PACKAGE = JSON.parse(fs.readFileSync('package.json')),
-    NAME = PACKAGE.name,
-    VERSION = PACKAGE.version,
-    SAVE_INTERVAL = 15, // candle size in seconds
-    LOG_INTERVAL = 60*60*24, // hr output period in seconds
-    TS_START = (new Date()).getTime(), // starting time in ms
-    PAIRSFILE = "pairsBinance.json", // name of file where pairs are stored
+const PAIRSFILE = "pairsBinance.json", // name of file where pairs are stored
     EXCHANGE = "Binance" // exchange name
-var BINANCE = new Binance(),
-    tsLast = {}, // object storing last timestamp for each pair
-    tsLastLog = 0, // integer storing the timestamp of the last log update
-    priceLast = {}, // object storing last price for each pair
-    trades = {}, // object containing lists of trades used to compile the next candles
-    subs = [] // list of currency pairs that im currently subscribed too
-
-// functions
-function _outLog() {
-    let hr = "#############",
-        ts = (new Date()).getTime(),
-        timeRunning = ts - TS_START,
-        daysRunning = timeRunning / (1000 * 60 * 60 * 24),
-        timeSinceLast = ts - tsLastLog
-    // get hr
-    if (tsLastLog == 0) {
-        hr = `${hr} ${NAME} v${VERSION} started up ${hr}`
-    } else {
-        hr += "#####"
-        if (timeSinceLast/1000 > LOG_INTERVAL) {
-            hr = `${hr} days since start: ${daysRunning.toFixed(2)} ${hr}`
-        } else return
-    }
-    // output log and update tsLastLog
-    console.log(hr)
-    tsLastLog = ts
-}
-
-function _startupMessage(exchange) {
-    console.log(`${exchange} WebSocket open.`)
-    setTimeout(() => {
-        console.log("Now collecting and storing candle data.")
-    }, 3000)
-}
-
-function _getSubs(filename) {
-    // update subscriptions from json file
-    // - get pairs from json then newSubs from pairs
-    let pairs = JSON.parse(fs.readFileSync(filename)),
-        newSubs = [],
-        pairToCurrencyPair = {}
-    for (base in pairs) {
-        for (i in pairs[base]) {
-            let asset = pairs[base][i],
-                currencyPair = base+"_"+asset,
-                pair = asset+base
-            pairToCurrencyPair[pair] = currencyPair
-            if (newSubs.indexOf(pair) < 0) {
-                newSubs.push(pair)
-            }
-        }
-    }
-    // - compare newSubs to subs
-    // -- unsub outdated subs
-    for (i in subs) {
-        let pair = subs[i],
-            currencyPair = pairToCurrencyPair[pair]
-        if (newSubs.indexOf(pair) < 0) {
-            delete trades[pair]
-            delete tsLast[pair]
-            delete priceLast[pair]
-            subs.splice(i, 1)
-            console.log(`Removed subscription to ${pair}.`)
-        }
-    }
-    // -- add new subs
-    for (i in newSubs) {
-        let pair = newSubs[i],
-            currencyPair = pairToCurrencyPair[pair]
-        if (subs.indexOf(pair) < 0) {
-            trades[pair] = []
-            tsLast[pair] = (new Date()).getTime()
-            priceLast[pair] = 0
-            subs.push(pair)
-            console.log(`Subscribed to ${pair}.`)
-        }
-    }
-}
-
-function _saveCandles(exchange) {
-    // convert trade data to candles and save to storage
-    // - backup trades, reset trades
-    let tradeData = {}
-    for (pair in trades) {
-        tradeData[pair] = trades[pair]
-        trades[pair] = []
-    }
-    // - cycle through subs
-    for (i in subs) {
-        let pair = subs[i],
-            // get timestamps
-            ts1 = tsLast[pair],
-            ts2 = (new Date()).getTime(),
-            // get trades for this pair only
-            tradeList = tradeData[pair],
-            // initialize ohlc and volume
-            ohlc = [0, 0, 0, 0],
-            volume = 0
-        tsLast[pair] = ts2
-        // get ohlc and volume
-        if (tradeList.length == 0) {
-            if (priceLast[pair] == 0) continue
-            let price = priceLast[pair]
-            ohlc = [price, price, price, price]
-        } else {
-            ohlc = [
-                tradeList[0].price,
-                tradeList[0].price,
-                tradeList[0].price,
-                tradeList[tradeList.length - 1].price
-            ]
-            for (j in tradeList) {
-                if (tradeList[j].price > ohlc[1]) ohlc[1] = tradeList[j].price
-                if (tradeList[j].price < ohlc[2]) ohlc[2] = tradeList[j].price
-                volume += tradeList[j].amount
-            }
-        }
-        // get candle
-        let candle = `${ts1},${ts2},`
-        for (j in ohlc) candle += `${ohlc[j]},`
-        candle += volume
-        // get path
-        let date = new Date(ts2),
-            day = date.getDate(),
-            month = date.getMonth() + 1,
-            year = date.getFullYear(),
-            path = "",
-            items = [`Data/`, `${exchange}/`, `${pair}/`, `${year}/`],
-            filename = `${month}-${day}.csv`
-        // make sure path exists one level at a time
-        for (i in items) {
-            path += items[i]
-            if (!fs.existsSync(path)) fs.mkdirSync(path)
-        }
-        path += filename
-        // append candle to path
-        fs.appendFileSync(path, candle + "\n")
-    }
-}
+var BINANCE = new Binance()
 
 // initialize websocket
-function _onTrade(pair, amount, price) {
-    trades[pair].push({
-        amount: amount,
-        price: price
-    })
-    priceLast[pair] = price
-}
-
-function _openWebSocket() {
+function openWS() {
     // get a list of pairs
     BINANCE.prices((err, ticker) => {
         if (err) throw err
@@ -202,27 +60,27 @@ function _openWebSocket() {
         BINANCE.websockets.trades(Object.keys(ticker), (trade) => {
             if (trade.e == "trade") {
                 let pair = trade.s
-                if (subs.indexOf(pair) > -1) {
+                if (lib.subs.indexOf(pair) > -1) {
                     let amount = parseFloat(trade.q),
                         price = parseFloat(trade.p)
-                    _onTrade(pair, amount, price)
+                    lib.onTrade(pair, amount, price)
                 }
             }
         })
-        _startupMessage(EXCHANGE)
+        lib.startupMessage(EXCHANGE)
     })
 }
 
 // script
-_outLog()
-_getSubs(PAIRSFILE)
-_openWebSocket()
+lib.outLog()
+lib.getSubs(PAIRSFILE)
+openWS()
 // - messages from initializing WS will appear here
 
 // - start collecting
 setInterval(() => {
     // save candles and refresh subscriptions every SAVE_INTERVAL
-    _outLog()
-    _saveCandles(EXCHANGE)
-    _getSubs(PAIRSFILE)
-}, SAVE_INTERVAL * 1000)
+    lib.outLog()
+    lib.saveCandles(EXCHANGE)
+    lib.getSubs(PAIRSFILE)
+}, lib.SAVE_INTERVAL * 1000)
