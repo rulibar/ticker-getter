45893c7ed7278c1fc73412b7e4b757ea22170aff
Ryan Ulibarri <38776589+rulibar@users.noreply.github.com>
2018-06-19 11:55:45 -0700

    Update scannerBinance to v1.1
    Cleared the v1.0 header

--- scannerBinance.js
+++ scannerBinance.js
@@ -4,42 +4,7 @@ Description: A candle data compiler and saver for Binance exchange. Periodically
 cycles through the pairs in pairsBinance.json, compile candle data, and store to
 the Data file. See README.md
 
-v1.0
-    v1.0.4
-    /- creation of scannerBinance.js and pairsBinance.js
-    v1.0.5
-    /- add version control to scannerBinance.js header
-    /- add the same pairs to pairsBinance as pairsPoloniex
-    /- import fs and node-binance-api
-    /- install node-binance-api into package
-    /- replicate scannerPoloniex code for Binance and use as the initial
-       functional release of scannerBinance
-    v1.0.6
-    /- make _getSubs more similar to Poloniex by tracking currencyPairs
-    v1.0.7
-    /- stop parsing float in ticker in _openWebSocket and simplify code
-    v1.0.8
-    /- remove allPairs from _openWebSocket and use Object.keys instead
-    /- change some comments
-    /- add _startupMessage function
-    /- add EXCHANGE to store the exchange name
-    /- use EXCHANGE as an argument when calling _startupMessage and
-       _saveCandles
-    /- add PAIRSFILE to track name of pairs file
-    /- use PAIRSFILE as argument in _getSubs()
-    /- copy _onTrade from scannerGdax
-    /- remove _onMessage and use _onTrade instead
-    /- refer to BINANCE as a var instead of constant
-    v1.0.9
-    /- import tgLib as lib
-    /- remove all constants except PAIRSFILE and EXCHANGE and call from lib
-    /- remove tsLastLog
-    /- remove _outLog and _startupMessage and call from lib
-    /- remove tsLast, priceLast, trades, subs
-    /- remove _getSubs and _saveCandles and call from lib
-    /- remove _onTrade and call from lib
-    /- rename _openWebSocket to openWS
-    /- stop importing fs
+v1.1
 
 */
 
