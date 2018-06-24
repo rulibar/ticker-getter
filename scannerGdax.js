88e2972f024a44ccf0ecfe91641b99ad2143f7df
Ryan Ulibarri <38776589+rulibar@users.noreply.github.com>
2018-06-23 22:41:59 -0700

    Update scannerGdax to v1.1.1
        /- use EXCHANGE in WS closed message instead of typing explicitly
        /- stop saying no response from server when WS closed
        /- combine log outputs into one line when WS closed
        /- replace log messages with the new lib.outMsg(str) to add a timestamp
        /- only log err if there was actually an error

--- scannerGdax.js
+++ scannerGdax.js
@@ -5,6 +5,12 @@ cycles through the pairs in pairsGdax.json, compile candle data, and store to
 the Data file. See README.md
 
 v1.1
+    v1.1.1
+    /- use EXCHANGE in WS closed message instead of typing explicitly
+    /- stop saying no response from server when WS closed
+    /- combine log outputs into one line when WS closed
+    /- replace log messages with the new lib.outMsg(str) to add a timestamp
+    /- only log err if there was actually an error
 
 */
 
@@ -19,8 +25,8 @@ var GDAX = new Gdax.PublicClient()
 // initialize websocket
 function setWSMethods(WS) {
     WS.on('error', (err) => {
-        console.log("Error:")
-        console.log(err)
+        lib.outMsg("Error!")
+        if (err) console.log(err)
     })
 
     WS.on('open', () => {
@@ -28,10 +34,7 @@ function setWSMethods(WS) {
     })
 
     WS.on('close', (res) => {
-        console.log("Gdax WebSocket closed.")
-        if (res == undefined) res = "No response from server."
-        console.log(res)
-        console.log("Trying to reopen the WebSocket...")
+        lib.outMsg(`${EXCHANGE} WebSocket closed. Trying to reconnect...`)
         setTimeout(() => openWS(), 1000)
     })
 
