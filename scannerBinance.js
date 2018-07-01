6f3d0faabbb9316f9a1622de73569091e1a4858a
Ryan Ulibarri <38776589+rulibar@users.noreply.github.com>
2018-07-01 13:37:55 -0700

    scannerBinance updated to v1.1.3
        /- add semicolons to make sure I'm using proper javascript syntax

--- scannerBinance.js
+++ scannerBinance.js
@@ -8,16 +8,18 @@ v1.1
     v1.1.2
     /- handle errors in BINANCE.prices in openWS() (in case of no internet or
        some other error.)
+    v1.1.3
+    /- add semicolons to make sure I'm using proper javascript syntax
 
 */
 
 // imports
 var lib = require('./tgLib'),
-    Binance = require('node-binance-api')
+    Binance = require('node-binance-api');
 
 const PAIRSFILE = "pairsBinance.json", // name of file where pairs are stored
-    EXCHANGE = "Binance" // exchange name
-var BINANCE = new Binance()
+    EXCHANGE = "Binance"; // exchange name
+var BINANCE = new Binance();
 
 // initialize websocket
 function openWS() {
@@ -25,36 +27,36 @@ function openWS() {
     // get a list of pairs
     BINANCE.prices((err, ticker) => {
         if (err) {
-            lib.outMsg("Error!")
-            console.log(err)
-            lib.outMsg("WS failed to open. Retrying...")
-            setTimeout(() => { openWS() }, 1000); return
+            lib.outMsg("Error!");
+            console.log(err);
+            lib.outMsg("WS failed to open. Retrying...");
+            setTimeout(() => { openWS() }, 1000); return;
         }
         // set up WS
         BINANCE.websockets.trades(Object.keys(ticker), (trade) => {
             if (trade.e == "trade") {
-                let pair = trade.s
+                let pair = trade.s;
                 if (lib.subs.indexOf(pair) > -1) {
                     let amount = parseFloat(trade.q),
-                        price = parseFloat(trade.p)
-                    lib.onTrade(pair, amount, price)
+                        price = parseFloat(trade.p);
+                    lib.onTrade(pair, amount, price);
                 }
             }
-        })
-        lib.startupMessage(EXCHANGE)
-    })
+        });
+        lib.startupMessage(EXCHANGE);
+    });
 }
 
 // script
-lib.outLog()
-lib.getSubs(PAIRSFILE)
-openWS()
+lib.outLog();
+lib.getSubs(PAIRSFILE);
+openWS();
 // - messages from initializing WS will appear here
 
 // - start collecting
 setInterval(() => {
     // save candles and refresh subscriptions every SAVE_INTERVAL
-    lib.outLog()
-    lib.saveCandles(EXCHANGE)
-    lib.getSubs(PAIRSFILE)
-}, lib.SAVE_INTERVAL * 1000)
+    lib.outLog();
+    lib.saveCandles(EXCHANGE);
+    lib.getSubs(PAIRSFILE);
+}, lib.SAVE_INTERVAL * 1000);
